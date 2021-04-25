d76058f2ad4776764576637171a04b2872e340c5
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-04-25 13:40:48 +0100

    test views for route for shipment

--- shipment/urls.py
+++ shipment/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from shipment import views as shipment_views
 
-app_name = 'shop'
+# app_name = 'shop'
 
 urlpatterns = [
     path('', shipment_views.index, name='shipment_index'),
     path('about/', shipment_views.about, name='shipment_about'),
-    path('<int:id>/<slug:slug>/', shipment_views.contact, name='shipment_contact'),   
+    path('conact/', shipment_views.contact, name='shipment_contact'), 
+    path('tracking/', shipment_views.track, name='shipment_track'),
+    path('service/', shipment_views.contact, name='shipment_service'),  
 
 ]
