a4776573aab285d338c033b02a897a6e8290aa24
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-05-22 00:33:35 +0100

    added model to admin

--- shipment/admin.py
+++ shipment/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import ItemSender, ItemDetail, ItemReciever
 
-# Register your models here.
+
+class ItemDetailInline(admin.TabularInline):
+    model = ItemDetail
+    raw_id_fields = ['item_sender']
+
+
+@admin.register(ItemSender)
+class ItemSenderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'company', 'address','postal_code', 'country', 'city', 'date_sent']
+    list_filter = ['user', 'country', 'date_sent']
+    model = ItemDetail
+
+
+@admin.register(ItemReciever)
+class ItemRecieverAdmin(admin.ModelAdmin):
+    list_display = ['sender', 'fullname', 'address','postal_code', 'country', 'city', 'date_created']
+    list_filter = ['sender', 'date_created', 'country']
+    list_editable = ['date_created', 'country']
