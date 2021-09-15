f5e2bad8f81aa9ffd7258a6b367bf79f2c077ceb
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-09-15 16:56:17 +0100

    updated view, changed tracking name from track to track_item

--- shipment/views.py
+++ shipment/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, HttpResponse
+from shipment.forms import ItemTrackForm
+from shipment.models import ItemDetail
 
 # Create your views here.
 
@@ -10,8 +12,19 @@ def index (request):
 def about (request):
     return render(request, 'shipment/about.html')
 
-def track (request):
-        return render(request, 'shipment/track.html')
+def track_item (request):
+    form = ItemTrackForm
+    if 'q' in request.GET:
+        form = ItemTrackForm(request.GET)
+        if form.is_valid():
+            q = form.cleaned_data['q']
+            item = ItemDetail.objects.get(item_code=q)
+            
+    context = {
+        'form': form,
+        'item': item
+    }
+    return render(request, 'shipment/track.html', context)
 
 def services (request):
     return render(request, 'shipment/services.html')
