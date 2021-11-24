7d4b5168f3c8d0eb86df668cb7b2f61baba7a3b2
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-11-24 16:25:38 +0100

    added routing for contact form

--- shipment/views.py
+++ shipment/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from django.http import JsonResponse, HttpResponse
-from shipment.forms import ItemTrackForm
+from shipment.forms import ItemTrackForm, ContactForm
 from shipment.models import ItemDetail
 
 # Create your views here.
@@ -31,4 +32,21 @@ def services (request):
     return render(request, 'shipment/services.html')
 
 def contact (request):
-    return render(request, 'shipment/contact.html')
+    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            print("Success")
+            messages.success(request, f"Recipe for {name} added successfully")
+            return redirect("shipment_index")
+        else:
+            print("Failed form submission")
+    
+
+    context = {
+        'form': form
+    }                           
+    return render(request, 'shipment/contact.html', context)
