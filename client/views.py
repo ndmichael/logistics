470c5bd28125fe6bf5098f19405a5f7db8bd27bb
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-11-27 17:47:27 +0100

    updated client templates and views

--- client/views.py
+++ client/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def dashboard(request, username):
+    context = {}
+    return render(request, 'account/user/dashboard.html', context)
