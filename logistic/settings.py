da01cdffe27c8d1260386df64846d1b3937d4097
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-11-28 16:02:22 +0100

    added test

--- logistic/settings.py
+++ logistic/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-27*_p^$)pg6)=0arhh_eb4mq+%3s#d!o2*vch9dxj&vp_&hi=6'
-SECRET_KEY =os.environ.get("SECRET_KEY_APP")
+SECRET_KEY = 'django-insecure-27*_p^$)pg6)=0arhh_eb4mq+%3s#d!o2*vch9dxj&vp_&hi=6'
+# SECRET_KEY =os.environ.get("SECRET_KEY_APP")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
