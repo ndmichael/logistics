5ad4470dfbff8c26784f5049fae8391bd1b8df8e
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-11-28 16:53:10 +0100

    test4

--- logistic/settings.py
+++ logistic/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-27*_p^$)pg6)=0arhh_eb4mq+%3s#d!o2*vch9dxj&vp_&hi=6'
-SECRET_KEY = os.environ.get("SECRET_KEY_APP")
-print(SECRET_KEY)
+SECRET_KEY = 'django-insecure-27*_p^$)pg6)=0arhh_eb4mq+%3s#d!o2*vch9dxj&vp_&hi=6'
+# SECRET_KEY = os.environ.get("SECRET_KEY_APP")
+# print(SECRET_KEY)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
+DEBUG=True
 
 
 ALLOWED_HOSTS = ['sdelivery.herokuapp.com', '127.0.0.1', 'localhost']
