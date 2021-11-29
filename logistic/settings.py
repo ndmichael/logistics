b8479450b22943f7f3068a50f0561def3b051fe0
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-11-29 22:42:49 +0100

    fixed django debug test2

--- logistic/settings.py
+++ logistic/settings.py
@@ -190,6 +190,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID_MIKE")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY_MIKE")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_SDELIVERY")
+print(AWS_STORAGE_BUCKET_NAME)
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_REGION_NAME = "us-east-2"
@@ -197,8 +198,8 @@ AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 AWS_DEFAULT_UCL = None
 
-if DEBUG:
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 
