83eaaae98e8c6381b70e76ffb5d988ec5420b5c5
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-11-29 23:46:23 +0100

    fixing debug variables

--- logistic/settings.py
+++ logistic/settings.py
@@ -198,8 +198,8 @@ AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 AWS_DEFAULT_UCL = None
 
-if not DEBUG:
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 
