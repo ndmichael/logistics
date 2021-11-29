653f4540d306a23ef5315693a2a21106fe69ca2d
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-11-29 23:09:38 +0100

    fixing botoS3

--- logistic/settings.py
+++ logistic/settings.py
@@ -186,11 +186,10 @@ ACCOUNT_LOGOUT_REDIRECT_URL = "account_login"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID_MIKE")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY_MIKE")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_SDELIVERY")
-print(AWS_STORAGE_BUCKET_NAME)
+
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_S3_REGION_NAME = "us-east-2"
@@ -198,8 +197,9 @@ AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 AWS_DEFAULT_UCL = None
 
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 
